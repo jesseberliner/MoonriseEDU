5842fd9e09f988bd51c56c452d3a3b77df0d36b5
JesseBerliner <jesseberliner@gmail.com>
2018-05-29 16:23:22 -0400

    Initial commit

---
+++ src/main/java/com/example/moonriseedu/Controllers/MainController.java
@@ -0,0 +1,15 @@
+package com.example.moonriseedu.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController
+{
+    @RequestMapping({"/home","/"})
+    public String mainPage(Model model)
+    {
+        return "index";
+    }
+}
---
+++ src/main/java/com/example/moonriseedu/MoonriseEduApplication.java
@@ -0,0 +1,12 @@
+package com.example.moonriseedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonriseEduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonriseEduApplication.class, args);
+	}
+}
